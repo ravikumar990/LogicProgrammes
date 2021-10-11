6109a39777032f1800f599eafe10189e5b01550d
ravi <ravi@gmail.com>
2021-10-11 08:32:47 +0530

    code for tictactoe

---
+++ TicTacToe.java
@@ -0,0 +1,15 @@
+public class TicTacToe {
+
+	public static void main(String[]args){
+      
+     System.out.println("play game");
+         CreateBoard();
+}
+ public static void main CreatBoard(){
+     char[] board = new Char[10];
+     for (int i=1; i<CreateBoard,length; i++ ) {
+     CreateBoard [i];
+     }
+
+   }
+ 
